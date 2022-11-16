5c5b06adff2d2b8b940b44e20cc7f97c38f0c48c
roee <roee1160@gmail.com>
2022-11-16 22:18:34 +0200

    added more input files

--- test_folder_proj1/test.java
+++ test_folder_proj1/test.java
@@ -152,7 +152,7 @@ static Scanner in = new Scanner(System.in);
 
 	String path = in.next();
 
-	for(int i =1; i<=4; i++) {
+	for(int i =1; i<=35; i++) {
 
 
 	/** TRYING TEST1 **/
@@ -208,11 +208,11 @@ static Scanner in = new Scanner(System.in);
 
 
 
-	System.out.println("Passed tests: "+passed_count+ " out of 4");
+	System.out.println("Passed tests: "+passed_count+ " out of 35");
 
 	
 
-	if(passed_count == 4)
+	if(passed_count == 35)
 		System.out.println("Passed all the tests! Well done!");
 	
 	}// end- main
