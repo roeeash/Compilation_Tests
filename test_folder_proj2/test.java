6d6cf3e5bb5489b8c669545da82c641c691560e4
roee <roee1160@gmail.com>
2022-12-08 14:02:44 +0200

    added test 28

--- test_folder_proj2/test.java
+++ test_folder_proj2/test.java
@@ -143,9 +143,9 @@ static Scanner in = new Scanner(System.in);
 	//variables
 	int parsePassedCount = 0;
 	int otherPassedCount = 0;
-	int parseTests = 23;
+	int parseTests = 24;
 	int otherTests = 4;
-	int testNum = 27;
+	int testNum = 28;
 
 	try{
 	PrintWriter output_writer = new PrintWriter("../../Compilation_Tests/test_folder_proj2/most_recent_output.txt");
